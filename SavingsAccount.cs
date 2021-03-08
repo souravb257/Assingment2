63bff2dd492c12407fcac15b762e7879526fb624
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-03-08 20:49:25 +0600

    Add files via upload

---
+++ SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assingment2
+{
+    class SavingsAccount:BankAccount
+    {
+        //fields
+        private int interestSavings;
+        private int minBalance;
+        private int timesWithdrawn;
+
+        //properties
+        public int Interest
+        {
+            get { return this.interestSavings; }
+        }
+        private int TimesWithdrawn
+        {
+            get { return this.timesWithdrawn; }
+        }
+        //constructors
+        public Savings(double balance) : base()
+        {
+            interestSavings = 10;
+            this.balance = balance;
+            accountType = "Savings Account";
+
+        }
+    }
+}
